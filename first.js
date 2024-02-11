20f7dd4b34d218f53c35e794e2a6e371aecbb578
Ashutosh Tandon <b23252@students.iitmandi.ac.in>
2024-02-11 15:11:59 +0530

    Updated

---
+++ first.js
@@ -0,0 +1,18 @@
+window.onscroll = function() {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    var btn = document.getElementById("goToTopBtn");
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      btn.style.display = "block";
+    } else {
+      btn.style.display = "none";
+    }
+  }
+
+  function goToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+  }
